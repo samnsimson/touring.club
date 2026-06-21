24a35d0844d622a56e1136cb1cbef1c988ea7257
samnsimson <samnishanth01@gmail.com>
2026-06-21 19:33:31 -0400

    swagger lib init

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,19 +1,15 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { validateEnv } from '@tc/config';
+import { bootstrapApplication } from '@tc/core';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    const globalPrefix = 'api';
-    app.setGlobalPrefix(globalPrefix);
-    const port = process.env.PORT || 3000;
-    await app.listen(port);
-    Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+    const env = validateEnv(process.env);
+    await bootstrapApplication({
+        globalPrefix: 'api',
+        rootModule: AppModule,
+        port: env.AUTH_SERVICE_PORT ?? 3000,
+        swagger: { title: 'Auth Service', description: 'Auth Service API' },
+    });
 }
 
 bootstrap();
