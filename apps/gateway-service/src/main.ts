d67c18bd35134e40286c4f28f3141cde71128948
samnsimson <samnishanth01@gmail.com>
2026-06-21 16:33:06 -0400

    save

--- apps/gateway-service/src/main.ts
+++ apps/gateway-service/src/main.ts
@@ -1,19 +1,14 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { bootstrapApplication } from '@tc/core';
 import { AppModule } from './app/app.module';
+import { validateEnv } from '@tc/config';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    const globalPrefix = 'api';
-    app.setGlobalPrefix(globalPrefix);
-    const port = process.env.PORT || 3000;
-    await app.listen(port);
-    Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+    const env = validateEnv(process.env);
+    await bootstrapApplication({
+        rootModule: AppModule,
+        port: env.PORT ?? 3000,
+        globalPrefix: 'api',
+    });
 }
 
 bootstrap();
--- library/core/src/lib/bootstrap/index.ts
+++ library/core/src/lib/bootstrap/index.ts
@@ -3,8 +3,9 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { composeRootModule } from './compose-root-module';
 import { BootstrapApplicationOptions } from './contract';
 import { validateEnv } from '@tc/config';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
 import { composeHealthRoutes } from './health.routes';
+import * as cookieParser from 'cookie-parser';
 
 export const bootstrapApplication = async ({ rootModule, port, globalPrefix = 'api', configure }: BootstrapApplicationOptions) => {
     const env = validateEnv(process.env);
@@ -12,12 +13,16 @@ export const bootstrapApplication = async ({ rootModule, port, globalPrefix = 'a
     const app = await NestFactory.create<NestExpressApplication>(composeRootModule(rootModule));
 
     app.setGlobalPrefix(globalPrefix);
+    app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1', prefix: 'v' });
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
+    app.use(cookieParser());
 
     composeHealthRoutes(app, globalPrefix);
     if (configure) await configure(app);
 
     await app.listen(port, () => {
-        logger.log(`🚀 ${env.NODE_ENV.toUpperCase()} service is running on: http://localhost:${port}/${globalPrefix}`);
+        const ENV = env.NODE_ENV.toUpperCase();
+        const SERVER_URL = `http://${env.HOST}:${port}/${globalPrefix}`;
+        logger.log(`🚀 ${ENV} service is running on: ${SERVER_URL}`);
     });
 };
