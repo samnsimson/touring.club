d36ffe5fecf466ecf3d64931c9089eba49b934e3
samnsimson <samnishanth01@gmail.com>
2026-06-21 03:52:50 -0400

    test auth generate

--- library/auth/src/lib/auth.config.ts
+++ library/auth/src/lib/auth.config.ts
@@ -3,8 +3,10 @@ import { typeormAdapter } from './adapter/auth.adapter';
 import { dataSource } from './auth.datasource';
 
 export const auth = betterAuth({
-    database: typeormAdapter(dataSource, {
-        usePlural: true,
-        transaction: true,
-    }),
+    name: 'Touring Club',
+    baseURL: 'http://localhost:3000',
+    basePath: '/api/auth',
+    secret: process.env.BETTER_AUTH_SECRET,
+    database: typeormAdapter(dataSource, { usePlural: true, transaction: true }),
+    emailAndPassword: { enabled: true, requireEmailVerification: true, autoSignIn: true },
 });
