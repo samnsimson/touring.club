f5c82139222cb9a8c20d6e9eb18863b91e003bf2
samnsimson <samnishanth01@gmail.com>
2026-06-29 12:15:59 -0400

    api client generated

--- library/shared/api-client/sciripts/client.config.ts
+++ library/shared/api-client/sciripts/client.config.ts
@@ -2,8 +2,28 @@ import { createClient } from '@hey-api/openapi-ts';
 
 createClient([
     {
-        input: { watch: true, path: 'apps/backend/auth-service/openapi/auth-service.openapi.json' },
-        output: { path: 'src/lib/auth-service-client' },
+        input: { watch: false, path: 'apps/backend/auth-service/openapi/auth-service.openapi.json' },
+        output: { path: 'library/shared/api-client/src/lib/auth-service-client', header: (ctx) => ['/* eslint-disable */', ...ctx.defaultValue] },
+        plugins: [{ name: '@hey-api/typescript' }, { name: '@hey-api/sdk', auth: true, operations: { strategy: 'single' } }],
+    },
+    {
+        input: { watch: false, path: 'apps/backend/users-service/openapi/users-service.openapi.json' },
+        output: { path: 'library/shared/api-client/src/lib/users-service-client', header: (ctx) => ['/* eslint-disable */', ...ctx.defaultValue] },
+        plugins: [{ name: '@hey-api/typescript' }, { name: '@hey-api/sdk', auth: true, operations: { strategy: 'single' } }],
+    },
+    {
+        input: { watch: false, path: 'apps/backend/trips-service/openapi/trips-service.openapi.json' },
+        output: { path: 'library/shared/api-client/src/lib/trips-service-client', header: (ctx) => ['/* eslint-disable */', ...ctx.defaultValue] },
+        plugins: [{ name: '@hey-api/typescript' }, { name: '@hey-api/sdk', auth: true, operations: { strategy: 'single' } }],
+    },
+    {
+        input: { watch: false, path: 'apps/backend/messaging-service/openapi/messaging-service.openapi.json' },
+        output: { path: 'library/shared/api-client/src/lib/messaging-service-client', header: (ctx) => ['/* eslint-disable */', ...ctx.defaultValue] },
+        plugins: [{ name: '@hey-api/typescript' }, { name: '@hey-api/sdk', auth: true, operations: { strategy: 'single' } }],
+    },
+    {
+        input: { watch: false, path: 'apps/backend/notifications-service/openapi/notifications-service.openapi.json' },
+        output: { path: 'library/shared/api-client/src/lib/notifications-service-client', header: (ctx) => ['/* eslint-disable */', ...ctx.defaultValue] },
         plugins: [{ name: '@hey-api/typescript' }, { name: '@hey-api/sdk', auth: true, operations: { strategy: 'single' } }],
     },
 ]);
